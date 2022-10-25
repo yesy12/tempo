bdb0a1e5d057a5bbc2a14796a0e15f40794398f8
Alison <alisonvieira.av29@gmail.com>
2022-10-25 03:14:43 -0300

    Add public folder, style css for result, PAGE get and post

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
 
 require("dotenv").config();
 const app = express();
 const port = 9000;
 
+app.use("/static", express.static("public"));
+
+app.set("view engine","ejs");
+app.set("views", "./src/views");
+
 const link = `${process.env.link}Taboao%20da%20Serra&appid=${process.env.appid}${process.env.link_parametrs}`;
 
-app.get("/",async(req,res)=>{
+app.get("/",(req,res)=>{
+    res.render("indexGet")
+})
+
+app.post("/",async(req,res)=>{
+    console.log(req.body);
     const response = await axios.get(link);
-    console.log(response.data);
-    res.json({"message":response.data});
+    res.render("indexPost",{
+        data: response.data
+    })
 })
 
 app.listen(port,()=>{
