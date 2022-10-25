ef2cf64ebc3ccedfe99c6ea5e64417c49489ce21
Alison <alisonvieira.av29@gmail.com>
2022-10-25 03:31:08 -0300

    add try/catch in case not found city, not found city is wrong, get and post in two file and two routes, api with data is divergent

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const app = express();
 const port = 9000;
 
+
+//public files
 app.use("/static", express.static("public"));
 
+
+//Req file
+app.use(bodyParser.urlencoded({
+    extended : false
+}));
+app.use(bodyParser.json());
+
+//Config
 app.set("view engine","ejs");
 app.set("views", "./src/views");
 
-const link = `${process.env.link}Taboao%20da%20Serra&appid=${process.env.appid}${process.env.link_parametrs}`;
 
 app.get("/",(req,res)=>{
     res.render("indexGet")
 })
 
 app.post("/",async(req,res)=>{
-    console.log(req.body);
-    const response = await axios.get(link);
-    res.render("indexPost",{
-        data: response.data
-    })
+    let {city} = req.body;
+
+    city = city.replace(" ","%20");
+    
+    const link = `${process.env.link}${city}&appid=${process.env.appid}${process.env.link_parametrs}`;
+    console.log(link);
+    try{
+        const response = await axios.get(link);
+        
+        res.render("indexPost",{
+            data: response.data
+        })
+    }
+    catch{
+        res.send("City not found");
+    }
+
 })
 
 app.listen(port,()=>{
